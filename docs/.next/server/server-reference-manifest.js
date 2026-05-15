8b0abcfdb2d066efe508e446d2fef08914011296
ospinajuanp <juanpo996@gmail.com>
2026-05-14 20:42:01 -0500

    update paquetes/librerias

--- docs/.next/server/server-reference-manifest.js
+++ docs/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"59kUGmO44bPx+SUZvIIdDDPMxc0qt+DEI4O3Z77ImWw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
